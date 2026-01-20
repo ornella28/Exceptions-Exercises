5fc33953dee78efa8886684748b17918021456a9
ornella28 <angelnibaruta88@gmail.com>
2026-01-20 21:29:27 +0100

    Exercise 4

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,14 +1,19 @@
 package se.lexicon;
 
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     static void main() {
 
         //Exercise 1
-       /* Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         try {
             System.out.println("Enter the first number: ");
@@ -28,17 +33,17 @@ public class Main {
         finally {
             System.out.println("Operation completed.");
             scanner.close();
-        } */
+        }
 
         //Exercise 2
-        /*try {
+        try {
             int number = readNumberInRange(1, 100);
             System.out.println("Valid number entered: " + number);
         } catch (InputMismatchException e){
             System.err.println("Error: Invalid input. Please enter an integer.");
         } catch (OutOfRangeException e){
             System.err.println("Error:" + e.getMessage());
-        }*/
+        }
 
         // Exercise 3
         BankAccount account = new BankAccount(500.0);
@@ -50,6 +55,25 @@ public class Main {
             System.err.println("Error: " + e.getMessage());
         }
         System.out.println("Final Balance: " + account.getBalance());
+
+
+        //Exercise 4
+        Path path = Path.of("non_existent_file.txt");
+        try {// outer exception handling for file operations
+            List<String> lines = Files.readAllLines(path);// read all lines from the file
+            for (String line:lines){// if the file is read successfully, parse each line to integer
+                try {
+                    int value = Integer.parseInt(line.trim());
+                    System.out.println("Parsed integer: " + value);
+                } catch (NumberFormatException e){
+                    System.err.println("Error: Unable to parse line to integer: " + line);
+                }
+            }
+        } catch (NoSuchFileException e){
+            System.err.println("Error: File not found:" + e.getFile());
+        } catch (IOException e){// any other file-reading problems
+            System.err.println("Error: An I/O error occurred: " + e.getMessage());
+        }
 
 
 
