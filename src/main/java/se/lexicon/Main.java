351b9a7ce2333d9f9f02cdceea7d911e99cf9f18
ornella28 <angelnibaruta88@gmail.com>
2026-01-20 10:37:48 +0100

    Exercise 1

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,10 +1,35 @@
 package se.lexicon;
 
 
+import java.util.Scanner;
+
 public class Main {
     static void main() {
 
-        
+        //Exercise 1
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.println("Enter the first number: ");
+            int firstNumber = scanner.nextInt();
+
+            System.out.println("Enter the second number: ");
+            int secondNumber = scanner.nextInt();
+
+            int result = firstNumber / secondNumber;
+            System.out.println("Result: " + result);
+        }
+        catch (ArithmeticException e){
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+        finally {
+            scanner.close();
+        }
+
+        //Exercise 2
+
+
+
 
     }
 }
